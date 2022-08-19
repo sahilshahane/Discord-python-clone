486a563800f6fcd5f0afacb1eb9f62bd9bfcf62c
Sahil Shahane <sahilpshahane123@gmail.com>
2022-08-19 08:22:31 +0530

    boilerplate

---
+++ src/MainWindow.py
@@ -0,0 +1,25 @@
+from PySide2 import QtCore, QtWidgets, QtGui
+import sys
+import random
+
+class MainApplication(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,15 @@
-print("hii sahil")
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+
+from MainWindow import MainApplication as MyApplication
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyApplication()
+    widget.resize(414, 736)
+    widget.show()
+
+    sys.exit(app.exec_())
